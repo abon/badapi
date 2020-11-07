91b351f381db8fa3ccfaaacc72bbc9454f1dd12b
Abdulloh <formygithubacc@gmail.com>
2020-11-07 15:48:25 +0500

    final commit:heavy_check_mark:

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
-
+import 'swiper/swiper.scss';
+import CarouselCard from './CarouselCard'
+import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import CarouselCard from './CarouselCard'
-
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const API_URL = 'https://breakingbadapi.com/api/characters'
-
-
 
 const Carousel = () => {
 
@@ -19,7 +21,7 @@ const Carousel = () => {
         const fetchItems = async () => {
             const result = await axios(`https://breakingbadapi.com/api/characters`)
             console.log(result.data);
-            setItems(result.data)
+            setItems(result.data);
         }
         fetchItems()
     }, [])
@@ -29,16 +31,18 @@ const Carousel = () => {
         <div >
             <Swiper
                 spaceBetween={50}
-                slidesPerView={3}
+                slidesPerView={1}
                 navigation
                 pagination={{ clickable: true }}
                 scrollbar={{ draggable: true }}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide>{items.map((item) => (
-                    <CarouselCard item={item} />
-                ))}</SwiperSlide>
+                {items.map((item) => (
+                    <SwiperSlide>
+                        <CarouselCard item={item} />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     )
--- src/Carousel/CarouselCard.jsx
+++ src/Carousel/CarouselCard.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+import './CarouselCard.css'
 
 const CarouselCard = ({ item: { name, birthday, img, status, nickname, portrayed } }) => {
     return (
-        <div>
-            <Card >
-                <CardActionArea>
-                    <CardMedia
-                        style={{ height: 600, objectFit: 'contain' }}
-                        image={img}
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {birthday}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {nickname}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {portrayed}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+        <div className="card">
+            <div className="card__main">
+                <img className='card__image' src={img} alt="" />
+                <p className='card__name'>{name}</p>
 
-            </Card>
+            </div>
+            <div className="card__info">
+                {birthday}
+            </div>
         </div>
+
+
     )
 }
 
